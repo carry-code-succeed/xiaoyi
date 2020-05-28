ade8d89711a1a35f3be82392a5f7a2f7b01fdf90
。。 <oncwnuPVCGIc64Uisui5yDO8rtMM@git.weixin.qq.com>
2020-05-28 10:39:24 +0800

    完成卖家主页

--- miniprogram/pages/theysell/theysell.js
+++ miniprogram/pages/theysell/theysell.js
@@ -1,18 +1,57 @@
-// pages/theysell/theysell.js
+
 Page({
 
+  async my_ongoods(){
+   let that=this
+    wx.request({
+      url: 'https://www.campustransaction.xyz/Q_M/A_P_Q_O/',
+      data:{
+        User_id:that.data.seller_id
+      },
+      success(res){
+        console.log(res);
+        that.setData({
+          goodslist:res.data
+        })     
+      }
+    })
+  },
   /**
    * 页面的初始数据
    */
   data: {
-    sell_id:"",
+    seller_id:"",
+    seller_info:[],
+    goodslist:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let that=this
     console.log(options)
+    
+    this.setData({
+      seller_id:options.sell_id
+    })
+    this.my_ongoods()
+    wx.request({
+      url: 'https://www.campustransaction.xyz/Q_M/U_I_Q/',
+      data:{
+        User_id:this.data.seller_id
+      },
+      success(res1){
+        console.log(res1)
+        that.setData({
+          seller_info:res1.data
+        })
+        
+      },
+      fail(res){
+        console.log(res)
+      }
+    })
   },
 
   /**
